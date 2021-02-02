d9d77bc3494cc393174c730f1031133a2a08990a
Terrance Cazy <terrancecazy@MacBook-Pro.lan>
2021-02-02 12:18:59 -0600

    add state and handlers

---
+++ src/components/Form.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+
+class Form extends Component {
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       name: ''
+    }
+  }
+handleChange = e => {
+  this.setState({
+    name: e.target.value
+  })
+}
+
+  
+  render() {
+    return (
+      <div>
+        <form>
+          <input value={this.state.patient} type="text" placeholder="Enter patient name"/>
+          <button type="submit">Add Patient</button>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default Form;
